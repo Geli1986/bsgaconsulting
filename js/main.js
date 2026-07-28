934f8530fca8e8bea22cb70c74a5e57a8696aca0
Geli1986 <angel.bajo.sanchez@gmail.com>
2026-07-28 23:06:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,3 +72,18 @@ behavior:"smooth"
 });
 
 }
+document.querySelectorAll("a[href^='#']").forEach(anchor=>{
+
+anchor.addEventListener("click",function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+});
+
+});
