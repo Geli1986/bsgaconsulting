3de6ff628d6f6139e920603864903c22f410d8c2
Geli1986 <angel.bajo.sanchez@gmail.com>
2026-08-01 18:23:03 +0200

    Update main.js

--- v2/v2/js/main.js
+++ v2/v2/js/main.js
@@ -1,1 +1,17 @@
+window.addEventListener("scroll",()=>{
 
+    const header=document.querySelector(".header");
+
+    if(window.scrollY>30){
+
+        header.classList.add("scrolled");
+
+    }
+
+    else{
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
