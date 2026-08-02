12e8025578ee0147bcef10e662449b4ca4f10e2c
Geli1986 <angel.bajo.sanchez@gmail.com>
2026-08-02 11:49:41 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
-console.log("BSGA Consulting loaded");
+/* ==========================================
+   HEADER ON SCROLL
+========================================== */
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 30) {
+
+        header.classList.add("scrolled");
+
+    } else {
+
+        header.classList.remove("scrolled");
+
+    }
+
+});
+
+
+/* ==========================================
+   FADE IN ON SCROLL
+========================================== */
+
+const observer = new IntersectionObserver(
+
+(entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},
+
+{
+
+    threshold:0.15
+
+}
+
+);
+
+document.querySelectorAll(".fade").forEach(element=>{
+
+    observer.observe(element);
+
+});
